b575e3f6fe52ce9d967ded4953f8c88070993656
Gleyson Barroso <g3l2e1@gmail.com>
2021-08-09 00:17:07 -0300

    alt: make select test

---
+++ test/LPQueryBuilderTest.php
@@ -0,0 +1,47 @@
+<?php
+
+require_once('..\LPQueryBuilder.php');
+require_once('..\SqlFormatter.php');
+class_alias('LPQueryBuilder', 'QB');
+
+class LPQueryBuilderTest {
+
+	public function __construct() {
+		
+	}	
+
+	public function selectAll() {
+		$query = QB::table('GbUsuario')
+			->join('GbCriador ON GbCriador.Id = GbUsuario.CriadoPor')
+			->leftJoin('GbModificador ON GbModificador.Id = GbUsuario.ModifPor')
+			->select(
+				'GbUsuario.Id as id',
+				'GbUsuario.Nome as nome',
+				'GbUsuario.FuncionarioId as funcionarioId',
+				'GbUsuario.CriadoEm as criadoEm',
+				'GbUsuario.CriadoPor as criadorId',
+				'GbCriador.Nome as criador',
+				'GbUsuario.ModifEm as modificadoEm',
+				'GbUsuario.ModifPor as modificadorId',
+				'GbModificador.Nome as modificador',
+				'GbUsuario.Ativo as ativo'
+			)
+			->where(
+				'GbUsuario.Ativo = 1',
+				"GbUsuario.Nome like '%ala%'"
+			)
+			->groupBy(
+				'GbUsuario.Nome', 'GbUsuario.Ativo'
+			)
+			->orderBy(
+				'GbUsuario.Nome', 'GbUsuario.Ativo ASC'
+			)
+			->paginate(10, 1);
+
+		print SqlFormatter::format($query->sqlCount());
+		print SqlFormatter::format($query->sql());
+	}
+
+	
+}
+
---
+++ test/index.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once('LPQueryBuilderTest.php');
+
+$test = new LPQueryBuilderTest();
+print $test->selectAll(); print '<br><br>';
